d524506e454c381ad9b362f4ee642e55f7ed4506
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-03-10 15:01:59 -0300

    churrascometro first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">🥩 CHURRASCÔMETRO 🥩</h1>
+
+<div class="demonstracao">
+    <h2>Demonstração: </h2>
+    <img src="./images/churrascometro-foto.png">
+    <img src="./images/churrascometroGif.gif">
+</div>
+
+<h1 align="center">💻 Projeto:</h1>
+
+<p>O projeto Churrascômetro foi feito com o intuito de ajudar o povo que gosta de um churrasco, mas não sabe a quantidade exata de carne, cerveja e bebidas para comprar. A aplicação também leva em consideração a presença de crianças e adapta as quantidades necessárias de carne e bebidas de acordo com o número de crianças.</p>
+<p>A duração do churrasco também é um fator importante para determinar a quantidade de comes e bebes a serem compradas. Se o evento tiver uma duração maior do que 6 horas, esse quantidade vai ser adaptada de acordo com essa duração.</p>
+<p>Foram utilizados os seguintes critérios:</p>
+<ul>
+    <li>Carne - 400g por pessoa. + de 6hrs 650g</li>
+    <li>Cerveja - 1200ml por pessoa. + de 6hrs 2000ml</li>
+    <li>Refrigerante/Água - 1000ml por pessoa + de 6hrs 1500ml</li>
+    <li>Crianças valem por 1/2</li>
+</ul>
+
+<h1>Tecnologias utilizadas: </h1>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+</ul>
---
+++ script.js
@@ -0,0 +1,47 @@
+// Carne - 400g por pessoa. + de 6hrs 650g
+// Cerveja - 1200ml por pessoa. + de 6hrs 2000ml
+// Refrigerante/Água - 1000ml por pessoa + de 6hrs 1500ml
+// crianças valem por 1/2
+
+const inputElementAdults = document.getElementById("adults")
+const inputElementKids = document.getElementById("kids")
+const inputElementDuration = document.getElementById("duration")
+const result = document.querySelector("div .result")
+
+function calculate() {
+    const numberOfAdults = inputElementAdults.value
+    const numberOfKids = inputElementKids.value
+    const durationTime = inputElementDuration.value
+
+    const meatAmount = (meatPP(durationTime) * numberOfAdults) + (meatPP(durationTime) * numberOfKids) / 2
+    const beerAmount = beerPP(durationTime) * numberOfAdults
+    const beverageAmount = (beveragePP(durationTime) * numberOfAdults) + (beveragePP(durationTime) * numberOfKids) / 2
+ 
+    result.innerHTML = `<p>|${Math.ceil(meatAmount / 1000)}kg de Carne</p>`
+    result.innerHTML += `<p>|${Math.ceil(beerAmount / 1000)}L de Cerveja</p>`
+    result.innerHTML += `<p>|${Math.ceil(beverageAmount / 1000)}L de Bebidas (Água / Refris)</p>`
+}
+
+function meatPP(duration) {
+    if (duration >= 6) {
+        return 650
+    } else {
+        return 400
+    }
+}
+
+function beerPP(duration) {
+    if (duration >= 6) {
+        return 1200
+    } else {
+        return 2000
+    }
+}
+
+function beveragePP(duration) {
+    if (duration >= 6) {
+        return 1000
+    } else {
+        return 1500
+    }
+}
